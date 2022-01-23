643a74fade05801d44fa2ec1febc31ac70c52cf7
Alex <oleksandr.horbunov@vistaprint.com>
2022-01-23 18:28:37 +0100

    React Router worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <NotFound />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+  </BrowserRouter>
   );
 }
 
