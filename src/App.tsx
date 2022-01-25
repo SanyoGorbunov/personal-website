b253b1dc73224e4f6c662062ec150d627501fde9
Alex <oleksandr.horbunov@vistaprint.com>
2022-01-25 22:15:12 +0100

    PW-1 Page placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage/>} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
   </BrowserRouter>
